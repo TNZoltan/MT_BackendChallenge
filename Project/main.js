2cb5be5f82677602e3f67e600e64f0c2b5f3cb79
Unknown <zolt0077@edu.eal.dk>
2018-05-20 19:41:18 +0200

    Test file and code added

--- Project/main.js
+++ Project/main.js
@@ -1,7 +1,13 @@
-import {getTestString} from 'csv-parser'
+let fs = require('fs')
+let csvReader = require('csv-parser')
 
 function tester () {
-    console.log(getTestString())
+    let csvReaderObj = new csvReader ()
+    let string = fs.readFileSync ('sample.csv', 'utf8')
+    csvReaderObj.addData(string)
+    console.log(csvReaderObj.latest)
+    csvReaderObj.clearData()
+    console.log(csvReaderObj.latest)
 }
 
 tester()
